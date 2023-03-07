0865741048118098a392274795dadb9a078b56fe
Bruno Moura <brunomoura0509@hotmail.com>
2023-03-07 20:44:56 -0300

    Criação de mais classes.

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -31,5 +31,57 @@ class Automovel {
     
 }
 
+class Mesa {
+    
+    var cor: String = "Branca"
+    var numeroDeLugares: Int = 4
+    var marca: String = "Tramontina"
+    var materialPlastico: Bool = true
+    var numeroDeCadeiras: Int = 4
+    
+    func sentarNaCadeira() {
+        print("Setei na cadeira da mesa")
+    }
+    
+    func colocarComidaNaMesa() {
+        print("A mesa está posta")
+    }
+    
+}
 
+class Moto {
+    
+    var cor: String = "Vermelha"
+    var cilindrada: Int = 160
+    var marca: String = "Honda"
+    var eNova: Bool = true
+    var quantidadeDePassageiros: Int = 2
+    
+    func ligarFarolAlto() {
+        print("O Farol alto está ligado")
+    }
+    
+    func ligarAMoto() {
+        print("A moto está ligada")
+    }
+    
+}
+
+class Macbook {
+    
+    var cor: String = "Cinza espacial"
+    var modelo: String = "Air M!"
+    var marca: String = "Apple"
+    var eNovo: Bool = true
+    var quantidadeDeMemoriaRamEmGigaBytes: Int = 8
+    
+    func ligarMac() {
+        print("O Macbook está ligado")
+    }
+    
+    func buscarAtualizacoes() {
+        print("Buscando atualizações para o MacOS")
+    }
+    
+}
 
