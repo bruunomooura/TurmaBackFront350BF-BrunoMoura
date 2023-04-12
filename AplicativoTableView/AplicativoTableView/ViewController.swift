6a5d511a355b3e8e7cc3b10c0237843b665ff621
Bruno Moura <brunomoura0509@hotmail.com>
2023-04-11 22:19:47 -0300

    Criação do projeto.

---
+++ AplicativoTableView/AplicativoTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AplicativoTableView
+//
+//  Created by Bruno Moura on 11/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
