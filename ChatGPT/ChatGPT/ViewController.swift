3312085a03d18f82059e5eb83707ca686ee2dd43
Bruno Moura <brunomoura0509@hotmail.com>
2023-05-11 19:43:17 -0300

    Criação do projeto.

---
+++ ChatGPT/ChatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Bruno Moura on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
